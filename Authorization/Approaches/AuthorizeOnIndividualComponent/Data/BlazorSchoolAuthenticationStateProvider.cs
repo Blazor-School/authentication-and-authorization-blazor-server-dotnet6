650e0d20e0969e83128c35b1f3c0ed3895df709d
Red Wei <hevilhuy2@gmail.com>
2022-10-06 22:34:31 +0700

    Added authorization on individual component example.

---
+++ Authorization/Approaches/AuthorizeOnIndividualComponent/Data/BlazorSchoolAuthenticationStateProvider.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace AuthorizeOnIndividualComponent.Data;
+
+public class BlazorSchoolAuthenticationStateProvider : AuthenticationStateProvider, IDisposable
+{
+    private readonly BlazorSchoolUserService _blazorSchoolUserService;
+    public User CurrentUser { get; private set; } = new();
+
+    public BlazorSchoolAuthenticationStateProvider(BlazorSchoolUserService blazorSchoolUserService)
+    {
+        _blazorSchoolUserService = blazorSchoolUserService;
+        AuthenticationStateChanged += OnAuthenticationStateChangedAsync;
+    }
+
+    private async void OnAuthenticationStateChangedAsync(Task<AuthenticationState> task)
+    {
+        var authenticationState = await task;
+
+        if (authenticationState is not null)
+        {
+            CurrentUser = User.FromClaimsPrincipal(authenticationState.User);
+        }
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var principal = new ClaimsPrincipal();
+        var user = await _blazorSchoolUserService.FetchUserFromBrowserAsync();
+
+        if (user is not null)
+        {
+            var userInDatabase = _blazorSchoolUserService.LookupUserInDatabase(user.Username, user.Password);
+
+            if (userInDatabase is not null)
+            {
+                principal = userInDatabase.ToClaimsPrincipal();
+                CurrentUser = userInDatabase;
+            }
+        }
+
+        return new(principal);
+    }
+
+    public async Task LoginAsync(string username, string password)
+    {
+        var principal = new ClaimsPrincipal();
+        var user = _blazorSchoolUserService.LookupUserInDatabase(username, password);
+
+        if (user is not null)
+        {
+            await _blazorSchoolUserService.PersistUserToBrowserAsync(user);
+            principal = user.ToClaimsPrincipal();
+        }
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(principal)));
+    }
+
+    public async Task LogoutAsync()
+    {
+        await _blazorSchoolUserService.ClearBrowserUserDataAsync();
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new())));
+    }
+
+    public void Dispose() => AuthenticationStateChanged -= OnAuthenticationStateChangedAsync;
+}
---
+++ Authorization/Approaches/AuthorizeOnIndividualComponent/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+
+namespace AuthorizeOnIndividualComponent.Data;
+
+public class User
+{
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+
+    public ClaimsPrincipal ToClaimsPrincipal() => new(new ClaimsIdentity(new Claim[]
+    {
+        new (ClaimTypes.Name, Username),
+        new (ClaimTypes.Hash, Password),
+    }, "BlazorSchool"));
+
+    public static User FromClaimsPrincipal(ClaimsPrincipal principal) => new()
+    {
+        Username = principal.FindFirstValue(ClaimTypes.Name),
+        Password = principal.FindFirstValue(ClaimTypes.Hash)
+    };
+}
